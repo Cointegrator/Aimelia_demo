30ccb7b9d540cd035ebe63df1e9c27d6988e45fe
Mukhopadhyay <praneshmukherjee7@gmail.com>
2024-05-30 12:16:53 +0530

    Adding documentation to the endpoints

--- src/api/v1/task.py
+++ src/api/v1/task.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from pydantic import BaseModel
+from typing import List, Dict, Optional
 
-router = APIRouter(prefix='/task', tags=['Task Node'])
+router = APIRouter(prefix="/task", tags=["Task Node"])
 
-@router.get('/')
-async def test():
-    return {'data': 'data'}
+
+class EventRequest(BaseModel):
+    question: str
+    description: str
+    categories: Dict[str, List[str]]
+    initial_yes_price: Optional[float] = 50
+    initial_no_price: Optional[float] = 50
+
+
+@router.post("/")
+async def create_new_event(event: EventRequest):
+    """
+    Currently not implemented.
+    This endpoint will be used to create a new event in the system.
+    """
+    _ = event
+    return {
+        "status": status.HTTP_501_NOT_IMPLEMENTED,
+        "message": "Will be implemented shortly",
+    }
--- src/api/v1/validation.py
+++ src/api/v1/validation.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
-router = APIRouter(prefix='/validation', tags=['Validation Node'])
+router = APIRouter(prefix="/validation", tags=["Validation Node"])
 
-@router.get('/')
-async def test():
-    return {'data': 'data'}
+
+@router.get("/{condition_id}/validate")
+async def validate_event_by_condition_id():
+    """
+    **Currently not implemented.**
+
+    This endpoints sole responsiblity is to use the inference and data node
+    and the backend AI engine to automatically set or help the user
+    come to a conclusion for the event.
+    """
+    return {
+        "status": status.HTTP_501_NOT_IMPLEMENTED,
+        "message": "Will be implemented shortly",
+    }
