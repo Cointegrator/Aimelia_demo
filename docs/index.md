b7b76d527a1ae2fe18e234b5009092ea57a3c34c
Mukhopadhyay <praneshmukherjee7@gmail.com>
2024-06-05 11:29:50 +0530

    Adding local documentation

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# Aimelia
+
+Aimelia Network is a generative coevolution protocol for AI agents, which focuses on the evaluation of AI agents with game theory and bounty rewards.
+
+Aimelia reshapes the supply chain of personalized AI agents by creating a more decentralized, transparent, and rewardable internetwork.
--- src/config/configs.py
+++ src/config/configs.py
@@ -1,7 +1,7 @@
 DEBUG: bool = True
 
-BASE: str = 'https://clob.polymarket.com'
-AUTH_BASE: str = 'https://clob-staging.polymarket.com/'
+BASE: str = "https://clob.polymarket.com"
+AUTH_BASE: str = "https://clob-staging.polymarket.com/"
 CACHE: bool = True  # Local file based cache
 
-AUTO_BUY: bool = False
+AUTO_BUY: bool = True
